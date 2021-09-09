5eea5bc2294f367138969942926aaf31ec59f854
showboat051 <rashad.davis87@yahoo.com>
2021-09-09 01:47:27 -0500

    added getters& setters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,38 @@
 public class BankAccount {
-}
+
+
+    private String custName;
+    private int balance;
+    private int previousTransaction;
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getPreviousTransaction() {
+        return previousTransaction;
+    }
+
+    public void setPreviousTransaction(int previousTransaction) {
+        this.previousTransaction = previousTransaction;
+    }
+
+    public void init() {
+        System.out.println("Hello and Welcome");
+    }
+
+
+
+}// end of class
